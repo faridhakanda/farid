30749284073bdd6ce71b8c422a5e4bbff255bc93
farid-akanda <faridakanda1729@gmail.com>
2023-08-09 20:19:24 +0600

    fifth commit

--- src/app/group/page.tsx
+++ src/app/group/page.tsx
@@ -22,77 +22,77 @@ const Group = () => {
         <div className="bg-slate-100 text-slate-950 w-48 h-56 justify-center mx-auto rounded m-2">
         <div className="w-48 h-24 p-3 bg-orange-300 rounded">
             <Image className="mx-auto rounded-full bg-slate-200" src="/twitter.png" alt="vuser" width="48" height="48" />
-            <h1>Fariba Haque</h1>
+            <h1>Shafaet Asraf</h1>
         </div>
         <div className="p-2">
             <h1 className="text-sm">Software Engineer</h1>
-            <h1>Apple in California, USA</h1>
+            <h1>Meta in London, UK</h1>
             <h1 className="bg-sky-700 rounded-xl p-0.5">Connect</h1>
         </div>
         </div>
         <div className="bg-slate-100 text-slate-950 w-48 h-56 justify-center mx-auto rounded m-2">
         <div className="w-48 h-24 p-3 bg-orange-300 rounded">
             <Image className="mx-auto rounded-full bg-slate-200" src="/twitter.png" alt="vuser" width="48" height="48" />
-            <h1>Fariba Haque</h1>
+            <h1>Zaheed Sabur</h1>
         </div>
         <div className="p-2">
             <h1 className="text-sm">Software Engineer</h1>
-            <h1>Apple in California, USA</h1>
+            <h1>Google in California, USA</h1>
             <h1 className="bg-sky-700 rounded-xl p-0.5">Connect</h1>
         </div>
         </div>
         <div className="bg-slate-100 text-slate-950 w-48 h-56 justify-center mx-auto rounded m-2">
             <div className="w-48 h-24 p-3 bg-orange-300 rounded">
                 <Image className="mx-auto rounded-full bg-slate-200" src="/twitter.png" alt="vuser" width="48" height="48" />
-                <h1>Fariba Haque</h1>
+                <h1>Tamim Shahriar</h1>
             </div>
             <div className="p-2">
                 <h1 className="text-sm">Software Engineer</h1>
-                <h1>Apple in California, USA</h1>
+                <h1>Grab in Singapore</h1>
                 <h1 className="bg-sky-700 rounded-xl p-0.5">Connect</h1>
             </div>
         </div>
         <div className="bg-slate-100 text-slate-950 w-48 h-56 justify-center mx-auto rounded m-2">
             <div className="w-48 h-24 p-3 bg-orange-300 rounded">
                 <Image className="mx-auto rounded-full bg-slate-200" src="/twitter.png" alt="vuser" width="48" height="48" />
-                <h1>Fariba Haque</h1>
+                <h1>Zulkernine Mahmud</h1>
             </div>
             <div className="p-2">
                 <h1 className="text-sm">Software Engineer</h1>
-                <h1>Apple in California, USA</h1>
+                <h1>Google in California, USA</h1>
                 <h1 className="bg-sky-700 rounded-xl p-0.5">Connect</h1>
             </div>
         </div>
         <div className="bg-slate-100 text-slate-950 w-48 h-56 justify-center mx-auto rounded m-2">
             <div className="w-48 h-24 p-3 bg-orange-300 rounded">
                 <Image className="mx-auto rounded-full bg-slate-200" src="/twitter.png" alt="vuser" width="48" height="48" />
-                <h1>Fariba Haque</h1>
+                <h1>Evan Ahmed</h1>
             </div>
             <div className="p-2">
                 <h1 className="text-sm">Software Engineer</h1>
-                <h1>Apple in California, USA</h1>
+                <h1>Grab in Singapore</h1>
                 <h1 className="bg-sky-700 rounded-xl p-0.5">Connect</h1>
             </div>
         </div>
         <div className="bg-slate-100 text-slate-950 w-48 h-56 justify-center mx-auto rounded m-2">
             <div className="w-48 h-24 p-3 bg-orange-300 rounded">
                 <Image className="mx-auto rounded-full bg-slate-200" src="/twitter.png" alt="vuser" width="48" height="48" />
-                <h1>Fariba Haque</h1>
+                <h1>Sudipto Khor</h1>
             </div>
             <div className="p-2">
                 <h1 className="text-sm">Software Engineer</h1>
-                <h1>Apple in California, USA</h1>
+                <h1>Amazon in California, USA</h1>
                 <h1 className="bg-sky-700 rounded-xl p-0.5">Connect</h1>
             </div>
         </div>
         <div className="bg-slate-100 text-slate-950 w-48 h-56 justify-center mx-auto rounded m-2">
             <div className="w-48 h-24 p-3 bg-orange-300 rounded">
                 <Image className="mx-auto rounded-full bg-slate-200" src="/twitter.png" alt="vuser" width="48" height="48" />
-                <h1>Fariba Haque</h1>
+                <h1>Bishal Ahmed</h1>
             </div>
             <div className="p-2">
                 <h1 className="text-sm">Software Engineer</h1>
-                <h1>Apple in California, USA</h1>
+                <h1>Microsoft in California, USA</h1>
                 <h1 className="bg-sky-700 rounded-xl p-0.5">Connect</h1>
             </div>
         </div>
